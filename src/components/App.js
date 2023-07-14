65da0d207a0e1833fb11e316716da9b0629659f1
Fun-25 <muskandodmani760@gmail.com>
2023-07-14 19:51:42 +0530

    submitting code-Fri Jul 14 2023 19:51:34 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,13 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <div id="main">
+      <h2 data-ns-test="project">
+        Builder
+      </h2>
+      <p data-ns-test="project-description">It's an e-commerce web app</p>
+    </div>
+  );
 }
 
 
